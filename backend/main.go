dc2ff786b13adadf98fa66eacf90672c5e62f9ab
kc8 <kyle@cooperkyle.com>
2023-06-05 20:10:47 -0400

    update backend to better support ids for sensors rather than names

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -52,16 +54,17 @@ func (s *Services) populateTheSensors(g *gin.Context) {
 		})
 		return
 	}
-	s.sensors.sensors = make(map[SensorName]SensorData, 10)
+	s.sensors.sensors = make(map[SensorId]SensorData, 10)
 	for i := 0; i < sensorLen; i++ {
 		var currentSensor = json.Sensors[i]
-		name := SensorName{name: currentSensor.SensorName}
+		id := SensorId{id: currentSensor.Id}
 		measurement := currentSensor.Measurment
 		unit := currentSensor.Unit
-		s.sensors.sensors[name] = SensorData{
-			name:        name,
+		s.sensors.sensors[id] = SensorData{
+			name:        currentSensor.SensorName,
 			measurement: measurement,
 			unit:        unit,
+			id:          id,
 		}
 
 	}
@@ -88,22 +91,24 @@ func (s *Services) updateSensorData(g *gin.Context) {
 	}
 	for i := 0; i < sensorLen; i++ {
 		var currentSensor = json.Sensors[i]
-		name := SensorName{name: currentSensor.SensorName}
+		id := SensorId{id: currentSensor.Id}
 		measurement := currentSensor.Measurment
 		unit := currentSensor.Unit
+		name := currentSensor.SensorName
 
 		sensorData := SensorData{
 			measurement: measurement,
 			name:        name,
 			unit:        unit,
+			id:          id,
 		}
 		currentStatus := s.sensors.Update(sensorData)
-        log.Println("[INFO] Received request to update sensor with the following", sensorData);
+		log.Println("[INFO] Received request to update sensor with the following", sensorData)
 
 		// TODO don't fail right away, update sensors that are valid and
 		// then return states for each sensor updated
 		if currentStatus == false {
-			g.JSON(http.StatusOK, gin.H{"Update Sensor Data": currentStatus})
+			g.JSON(http.StatusBadRequest, gin.H{"Update Sensor Data": currentStatus})
 			return
 		}
 	}
@@ -114,20 +119,22 @@ func (s *Services) getAvailableSensors(g *gin.Context) {
 }
 
 func (s *Services) getSensorData(g *gin.Context) {
-	sensorName := g.Query("sensorName")
-	data, err := s.sensors.GetSensorData(SensorName{name: sensorName})
+	sensorId := g.Query("sensorId")
+	data, err := s.sensors.GetSensorData(SensorId{id: sensorId})
 	if err != nil {
-		g.JSON(http.StatusBadRequest, gin.H{"Could not find sensor": sensorName})
+		g.JSON(http.StatusBadRequest, gin.H{"Could not find sensor": sensorId})
 		return
 	}
-    serializedData := RawSensorData{
-		SensorName: data.name.name,
-		Id:         "",
-        Status:     "",
-        Unit: data.unit,
+	fmt.Println("Getting sensor Data", sensorId)
+	serializedData := RawSensorData{
+		SensorName: data.name,
+		Id:         data.id.id,
+		Status:     "",
+		Unit:       data.unit,
 		Measurment: data.measurement,
 	}
 
+	fmt.Println("Got", serializedData)
 	g.JSON(http.StatusOK, serializedData)
 	return
 }
@@ -135,19 +142,27 @@ func (s *Services) getSensorData(g *gin.Context) {
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"POST", "GET"},
+		AllowHeaders: []string{"*"},
+		//ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+	}))
+
 	r.Use(AuthWrapper())
 	services := Services{}
 
-    // since our rpi-drivers are messy.. for now I just want to update them here
-    sensorNames := []string{
-        "soil_moisture_plant_1",
-        "soil_moisture_plant_2",
-        "soil_temp_plant_1",
-        "soil_temp_plant_2",
-        "ambient_temp",
-        "ambient_humidity",
-        "ambient_pressure"}
-    services.sensors.InitSensors(sensorNames, len(sensorNames))
+	// since our rpi-drivers are messy.. for now I just want to update them here
+	sensorNames := []string{
+		"soil_moisture_plant_1",
+		"soil_moisture_plant_2",
+		"soil_temp_plant_1",
+		"soil_temp_plant_2",
+		"ambient_temp",
+		"ambient_humidity",
+		"ambient_pressure"}
+	services.sensors.InitSensors(sensorNames, len(sensorNames))
 
 	r.POST("/sendData", services.updateSensorData)
 	r.POST("/populateTheSensorsFromDevice", services.populateTheSensors)
